13b16c61eac73b8426e61a1d3ae93f8a9d5debec
Abidunnisa <abidunnisabegum@yahoo.com>
2025-10-25 23:07:37 +0530

    Updates

--- _next/static/chunks/pages/RenderPage-5ed0395419733229.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[138],{4397:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/RenderPage",function(){return u(635)}])}},function(n){n.O(0,[921,774,888,179],function(){return n(n.s=4397)}),_N_E=n.O()}]);
--- _next/static/chunks/pages/Webinars-80d757ea1364d897.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[644],{3655:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Webinars",function(){return u(9027)}])}},function(n){n.O(0,[921,774,888,179],function(){return n(n.s=3655)}),_N_E=n.O()}]);
