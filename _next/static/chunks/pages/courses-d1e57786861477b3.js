1b472bb4941ed4d0238617158906793a5828ad9f
Abidunnisa <abidunnisabegum@yahoo.com>
2025-10-25 22:40:58 +0530

    Updates

--- _next/static/chunks/pages/courses-d1e57786861477b3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[520],{5280:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/courses",function(){return u(3690)}])}},function(n){n.O(0,[921,774,888,179],function(){return n(n.s=5280)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/courses-f1d08a0200409415.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[520],{5280:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/courses",function(){return u(3690)}])}},function(n){n.O(0,[630,774,888,179],function(){return n(n.s=5280)}),_N_E=n.O()}]);
